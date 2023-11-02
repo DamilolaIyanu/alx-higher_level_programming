81cb81cf23ef1f240abc42f0ffc7c5f10fb36487
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-02 06:47:24 -0700

    task two simple calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-
-addResult = add(a, b)
-subResult = sub(a, b)
-mulResult = mul(a, b)
-divResult = div(a, b)
-
-print("{} + {} = {}".format(a, b, addResult))
-print("{} - {} = {}".format(a, b, subResult))
-print("{} * {} = {}".format(a, b, mulResult))
-print("{} / {} = {}".format(a, b, divResult))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
