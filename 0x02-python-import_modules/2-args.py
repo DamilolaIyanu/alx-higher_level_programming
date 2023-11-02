fbe68d3cd7264b4af0fc087e467c1d296a018c61
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-02 06:51:45 -0700

    counting and printing arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-arguments = sys.argv[1:]
-countAv = len(arguments)
-if countAv == 1:
-    print(f"{countAv} argument:")
-elif countAv > 1:
-    print(f"{countAv} arguments:")
-else:
-    print(f"{countAv} arguments.")
-
-for i, arg in enumerate(arguments, start=1):
-    print(f"{i}: {arg}")
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv[1:]
+    countAv = len(arguments)
+    if countAv == 1:
+        print(f"{countAv} argument:")
+    elif countAv > 1:
+        print(f"{countAv} arguments:")
+    else:
+        print(f"{countAv} arguments.")
+    for i, arg in enumerate(arguments, start=1):
+        print(f"{i}: {arg}")
