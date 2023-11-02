d91cca5bbe6ea50237ba027de51f742891db277a
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-02 12:01:59 -0700

    my calcultor

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     a = int (sys.argv[1])
     b = int (sys.argv[3])
     op = sys.argv[2]
@@ -18,3 +19,4 @@ if __name__ == "__main__":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
