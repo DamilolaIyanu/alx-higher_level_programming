4ac42f1d1cbd59a5f8865e03be49dbf7c55d2d0c
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-06 12:17:46 -0800

    changing a duplicate at an idex specified

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return copy(my_list)
-    else:
-        my_list_dup = my_list.copy()
-        my_list_dup[idx] = element
-        return my_list_dup
+
+    my_list_dup = my_list.copy()
+    my_list_dup[idx] = element
+    return my_list_dup
