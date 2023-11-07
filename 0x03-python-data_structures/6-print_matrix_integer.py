a9b58d9cf5cc2a0eada6c537cf636e4f66e1dedd
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-07 07:37:58 -0800

    printing a nested list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for row in matrix:
-        for numbers in row:
-            print("{:d}".format(numbers, end=" ")
-        print()
+        if len(row) == 0:
+            print()
+        for num in range(len(row)):
+            print("{:d}".format(row[num]), end="\n" if num is len(row) - 1 else " ")
