4d5461740d805142b9c044f66976b7428b6a65f6
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-07 07:08:22 -0800

    printing a nested list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for number in row:
-            print("{:d}".format(number), end=" ")
+        for numbers in row:
+            print("{:d}".format(numbers, end=" ")
         print()
