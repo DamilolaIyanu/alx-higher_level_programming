8b5f4e4143cffbcc17fe4855794964fbfa85f160
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-08 01:05:45 -0800

    deletes at an index without using pop

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if not my_list or idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        modList = []
+        for i in range(len(my_list)):
+            modList.append(my_list[i])
+
+    return modList
