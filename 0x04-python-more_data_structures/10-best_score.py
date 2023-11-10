f2cb78676ddba523003eaffe54a0a23706a77d59
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-10 03:45:10 -0800

    return value of biggest integer

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,11 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    biggest_value = 0
+    biggest_key = None
 
+    for k, v in a_dictionary.items():
+        if v > biggest_value:
+            biggest_value = v
+            biggest_key = k
+    return biggest_key
