b5cb7eb1be00a281ce0883484edefc18b6159f20
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-22 08:11:44 -0800

    function divides 2 integers

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    dividend = 0
+    try:
+        dividend = a/b
+    except ZeroDivisionError:
+        dividend = None
+    finally:
+        print("Inside result:{}".format(dividend))
+    return dividend
