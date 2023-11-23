e1547299b0b985a88c1a42258b6235cabd6f0342
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-23 10:04:49 -0800

    created a private instance attribute in the class after initializing and raising errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""An empty class module."""
+
+
+class Square:
+    """Defines the class Square."""
+    def __init__(self, size=0):
+        """Initializer
+
+        arguments:
+            size: the side of a square
+        raise:
+            TypeError: size is not a digit
+            ValueError: size is less than 0
+        """
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        self.__size = size
