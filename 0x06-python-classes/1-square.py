6475b5297b7683e3ee72acc7e44198e455552c75
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-23 09:29:59 -0800

    created a private instance attribute in the class after initializing

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""An empty class module."""
+
+
+class Square:
+    """Defines the class Square."""
+    def __init__(self, size):
+        """Initializer
+
+        arguments:
+            size: the side of a square
+        """
+        self.__size = size
+
