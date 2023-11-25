909f3727bf82026059007172485384f567a42325
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-11-25 10:06:27 -0800

    defines a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""An empty class module."""
+
+
+class Square:
+    """Defines the class Square."""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializer
+
+        arguments:
+            size: the side of a square
+        """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """raise:
+            TypeError: size is not a digit
+            ValueError: size is less than 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        """gettint the position
+        """
+        return self.__position
+
+    @position.setter
+    def position(self,value):
+        if (not isinstance(value, tuple) or
+               len(value) == 2 or
+               not all(isinstance(num, int) for num in value) or
+               not all(num >= 0 for num in value)):
+                   raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """area of the square
+        Return:
+            returns the square of the size
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints the square
+        """
+        if self.__size == 0:
+            print("")
+            return
+
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
