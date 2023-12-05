8eb7ebe59f75dbf9f60851232ced52375a0bfdf1
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-12-05 15:12:43 -0800

    inserts a line of text file of a specific string

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,14 +4,14 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """insert a line of text file"""
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding='utf-8') as f:
         llist = []
         while True:
             line = f.readline()
             if line == "":
                 break
             llist.append(line)
-            if search_strings inline:
+            if search_string in line:
                 llist.append(new_string)
     with open(filenamen, "w", encoding="utf-8") as f:
         f.writelines(llist)
