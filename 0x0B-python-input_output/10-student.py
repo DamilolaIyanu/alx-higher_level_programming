b12624da37c74d74ae5342a0b4e6e9b08026da19
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-12-05 14:16:45 -0800

    dict instance of class student defined further

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module for student class"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return the dict of clas student"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        ddict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                ddict[key] = value
+        return ddict
