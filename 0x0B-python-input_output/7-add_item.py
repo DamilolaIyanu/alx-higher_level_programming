32fc94a8a02ab7dbcae5aeccfe321c4811ad8362
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-12-05 13:42:46 -0800

    loading and saving from json file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,12 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-veclist = list(sys.argv[1:])
+arglist = list(sys.argv[1:])
 
 try:
     data = load_from_json_file('add_item.json') 
 except Exception:
     data = []
 
-data.extend(veclist)
+data.extend(arglist)
 save_to_json_file(data, 'add_item.json')
