eaa651fc4cf61cab88868db6b33ce540f4ae4e98
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-12-05 05:17:31 -0800

    adds item from a file to another file using json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,9 +10,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 veclist = list(sys.argv[1:])
 
 try:
-    old_data = load_from_json_file('add_item.json') 
+    data = load_from_json_file('add_item.json') 
 except Exception:
-    old_data = []
+    data = []
 
 data.extend(veclist)
-save_to_json_file(old_data, 'add_item.json')
+save_to_json_file(data, 'add_item.json')
