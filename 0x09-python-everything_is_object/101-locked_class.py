4fcb820cf42278ee887b6b30f76238f80b8dd095
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-12-13 04:25:28 -0800

    advance objects

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,5 +3,5 @@
 
 
 class LockedClass:
-    ""prevents the user from dynamically creating new instance attributes"""
+    """prevents the user dynamically creating new instance attributes"""
     __slot__ = ["first_name"]
